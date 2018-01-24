c95d23373a5e8f503ca668fe31bde4f5102a4f75
Bruno Coimbra <b-coimbra@hotmail.com>
2018-01-24 15:52:36 -0200

    split color codes into separate header

--- src/void.h
+++ src/void.h
@@ -4,14 +4,8 @@
 #include <string.h>
 #include <math.h>
 #include <assert.h>
+#include <colors.h>
 
-#define COLOR_RED     "\x1b[31m"
-#define COLOR_GREEN   "\x1b[32m"
-#define COLOR_YELLOW  "\x1b[33m"
-#define COLOR_BLUE    "\x1b[34m"
-#define COLOR_MAGENTA "\x1b[35m"
-#define COLOR_CYAN    "\x1b[36m"
-#define COLOR_RESET   "\x1b[0m"
 #define PI (float) 3.14159265
 #define len(x) (int)(sizeof(x) / sizeof(x[0]))
 #define echo(f, ...) fprintf(stderr, "%s\n", f, ##__VA_ARGS__)
